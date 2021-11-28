5992e20ad10de33bba69d3cc41437e3afb83a6e7
Ashish Tripathi <Ashish Tripathi@DESKTOP-TK6QOTK>
2021-11-28 20:05:13 +0530

    Add project files.

---
+++ second_crud/Controllers/EmpAdd.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using second_crud.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace second_crud.Controllers
+{
+    public class EmpAdd : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public EmpAdd(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var displaydata = _db.EmployeeTable.ToList();
+            return View(displaydata);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Create(NewEmpClass nec)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Add(nec);
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Index");
+
+            }
+            return View(nec);
+        }
+
+        public async Task <IActionResult>Edit(int?id)
+        {
+            if(id==null)
+            {
+                return RedirectToAction("Index");
+            }
+            var getempdetails = await _db.EmployeeTable.FindAsync(id);
+            return View(getempdetails);
+        }
+
+        [HttpPost]
+
+
+        public async Task<IActionResult>Edit(NewEmpClass nc)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Update(nc);
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(nc);
+        }
+
+        public async Task<IActionResult>Details(int ? id)
+        {
+            if (id == null)
+
+            {
+                return RedirectToAction("Index");
+            }
+            var getempdetails = await _db.EmployeeTable.FindAsync(id);
+            return View(getempdetails);
+        }
+
+        public async Task<IActionResult>Delete(int?id)
+        {
+            if(id==null)
+            {
+                return RedirectToAction("Index");
+            }
+            var getempdelete = await _db.EmployeeTable.FindAsync(id);
+            return View(getempdelete);
+        }
+        [HttpPost]
+
+        public async Task<IActionResult>Delete(int id)
+        {
+            var getempdelete = await _db.EmployeeTable.FindAsync(id);
+            _db.EmployeeTable.Remove(getempdelete);
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ second_crud/Models/NewEmpClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace second_crud.Models
+{
+    public class NewEmpClass
+    {
+        [Key]
+
+        [Display(Name = "Id")]
+        public int emp_id { get; set; }
+       
+
+
+        [Required(ErrorMessage = "Employee Name !!!!")]
+        [Display(Name = "Entert Name Here")]
+        public string enp_name { get; set; }
+
+
+        [Required(ErrorMessage = "Please Enter The Email !!!!")]
+        [Display(Name = "Entert Email Here")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+      
+
+        [Required(ErrorMessage ="Please Enter Real age")]
+        [Display(Name ="Age")]
+        [Range(20,50)]
+        public int age { get; set; }
+
+
+        [Required(ErrorMessage ="Enter Employee Salary")]
+        [Display(Name ="Salary")]
+        public decimal salary { get; set; }
+    }
+}
